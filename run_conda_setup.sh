46bcf7ca56af11d5223185cb5ef13f4d05c387c2
mike <mike@debian.home>
2025-07-02 10:50:43 +0200

    2025-07-02: small scale revision on setup

---
+++ run_conda_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 Helmholtz Centre for Geosciences
+#
+# SPDX-License-Identifier: EUPL-1.2
+
+# ==============================================================================
+# Script: run_venv_setup.sh
+# Purpose: Call the Python virtual environment setup script with parameters
+# Author: Mike Sips
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Default parameters (can be overridden via command line)
+# ------------------------------------------------------------------------------
+ENV_NAME=${1:-venv}
+REQUIREMENTS_YML=${2:-requirements.yml}
+
+# ------------------------------------------------------------------------------
+# Call the setup script located in setup/python
+# ------------------------------------------------------------------------------
+bash setup/conda/setup_conda.sh "$ENV_NAME" "$REQUIREMENTS_YML"
